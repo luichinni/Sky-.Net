23494e8208316618769e5dac8b75f14bce1b09a2
Luciano <lumacias11702@gmail.com>
2023-11-03 22:04:10 -0300

    Organización inicial

---
+++ CommandPattern/Comandos/PonerStandbyCmd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkyNet.CommandPattern;
+
+namespace SkyNet.CommandPattern.Comandos
+{
+    class PonerStandbyCmd : Comando
+    {
+    }
+}
