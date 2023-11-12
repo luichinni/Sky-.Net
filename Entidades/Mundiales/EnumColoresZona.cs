a01bf69a4cba755c3fcecef8344320a9aeaa68e1
Luciano <lumacias11702@gmail.com>
2023-11-12 15:49:41 -0300

    Enum Zonas y Colores (revisar)

---
+++ Entidades/Mundiales/EnumColoresZona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyNet.Entidades.Mundiales
+{
+    public enum EnumColoresZona
+    {
+        TerrenoBaldio = ConsoleColor.Gray,
+        Planicie = ConsoleColor.Green,
+        Bosque = ConsoleColor.DarkGreen,
+        SectorUrbano = ConsoleColor.DarkGray,
+        Lago = ConsoleColor.Cyan,
+        Vertedero = ConsoleColor.Yellow,
+        VertederoElectronico = ConsoleColor.DarkYellow,
+        Cuartel = ConsoleColor.Magenta,
+        SitioReciclaje = ConsoleColor.DarkMagenta
+    }
+}
---
+++ Entidades/Mundiales/EnumTiposDeZona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyNet.Entidades.Mundiales
+{
+    public enum EnumTiposDeZona
+    {
+        TerrenoBaldio,
+        Planicie,
+        Bosque,
+        SectorUrbano,
+        Lago,
+        Vertedero,
+        VertederoElectronico,
+        Cuartel,
+        SitioReciclaje
+    }
+}
