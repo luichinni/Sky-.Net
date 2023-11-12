989b1cdd041d90824e8613a0722f1c9a95fec20f
Luciano <lumacias11702@gmail.com>
2023-11-12 20:56:28 -0300

    Mas comandos

---
+++ CommandPattern/Comandos/CargarSimulacionCmd.cs
@@ -0,0 +1,21 @@
+﻿using SkyNet.Entidades.Mundiales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyNet.CommandPattern.Comandos
+{
+    public class CargarSimulacionCmd : Comando
+    {
+        public CargarSimulacionCmd(string nombre, string descripcion) : base(nombre, descripcion)
+        {
+        }
+
+        public override void Ejecutar(Mundo m, Cuartel c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
