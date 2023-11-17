2423b9fe2c5030f395d1f6742d679121b83ccaa1
Luciano <lumacias11702@gmail.com>
2023-11-16 23:03:19 -0300

    actualizacion config del program

--- Program.cs
+++ Program.cs
@@ -11,11 +11,54 @@ string seleccion;
 Mundo mundito = Mundo.GetInstance();
 Cuartel cuartelito = null;
 
+mundito.ExpansionZonal[(int)EnumTiposDeZona.Lago] = 10;
+mundito.ExpansionZonal[(int)EnumTiposDeZona.Planicie] = 0; // no hace falta porq el mundo inicia todo planicie
+mundito.ExpansionZonal[(int)EnumTiposDeZona.Bosque] = 12;
+mundito.ExpansionZonal[(int)EnumTiposDeZona.VertederoElectronico] = 2;
+mundito.ExpansionZonal[(int)EnumTiposDeZona.Vertedero] = 2;
+mundito.ExpansionZonal[(int)EnumTiposDeZona.SectorUrbano] = 3;
+mundito.ExpansionZonal[(int)EnumTiposDeZona.TerrenoBaldio] = 1;
+mundito.ExpansionZonal[(int)EnumTiposDeZona.SitioReciclaje] = 1; // no debe expandirse?
+mundito.ExpansionZonal[(int)EnumTiposDeZona.Cuartel] = 0; // no debe haber
+
+mundito.ExtensionZonal[(int)EnumTiposDeZona.Lago] = 10;
+mundito.ExtensionZonal[(int)EnumTiposDeZona.Planicie] = 0; // no hace falta porq el mundo inicia todo planicie
+mundito.ExtensionZonal[(int)EnumTiposDeZona.Bosque] = 10;
+mundito.ExtensionZonal[(int)EnumTiposDeZona.VertederoElectronico] = 3;
+mundito.ExtensionZonal[(int)EnumTiposDeZona.Vertedero] = 2;
+mundito.ExtensionZonal[(int)EnumTiposDeZona.SectorUrbano] = 10;
+mundito.ExtensionZonal[(int)EnumTiposDeZona.TerrenoBaldio] = 5;
+mundito.ExtensionZonal[(int)EnumTiposDeZona.SitioReciclaje] = 1; // no debe extenderse?
+mundito.ExtensionZonal[(int)EnumTiposDeZona.Cuartel] = 0; // no debe haber
+
+mundito.MaximaAparicion[(int)EnumTiposDeZona.Lago] = 25;
+mundito.MaximaAparicion[(int)EnumTiposDeZona.Planicie] = 0; // no hace falta porq el mundo inicia todo planicie
+mundito.MaximaAparicion[(int)EnumTiposDeZona.Bosque] = 25;
+mundito.MaximaAparicion[(int)EnumTiposDeZona.VertederoElectronico] = 12;
+mundito.MaximaAparicion[(int)EnumTiposDeZona.Vertedero] = 12;
+mundito.MaximaAparicion[(int)EnumTiposDeZona.SectorUrbano] = 12;
+mundito.MaximaAparicion[(int)EnumTiposDeZona.TerrenoBaldio] = 8;
+mundito.MaximaAparicion[(int)EnumTiposDeZona.SitioReciclaje] = 5; // no debe extenderse?
+mundito.MaximaAparicion[(int)EnumTiposDeZona.Cuartel] = 0; // no debe haber
+
+mundito.PrioridadZonal[0] = (int)EnumTiposDeZona.Planicie;
+mundito.PrioridadZonal[1] = (int)EnumTiposDeZona.Bosque;
+mundito.PrioridadZonal[2] = (int)EnumTiposDeZona.TerrenoBaldio;
+mundito.PrioridadZonal[3] = (int)EnumTiposDeZona.SectorUrbano;
+mundito.PrioridadZonal[4] = (int)EnumTiposDeZona.Vertedero;
+mundito.PrioridadZonal[5] = (int)EnumTiposDeZona.VertederoElectronico;
+mundito.PrioridadZonal[6] = (int)EnumTiposDeZona.SitioReciclaje;
+mundito.PrioridadZonal[7] = (int)EnumTiposDeZona.Cuartel;
+mundito.PrioridadZonal[8] = (int)EnumTiposDeZona.Lago;
+
 /// SetComandos -> PODRIA HACERSE DIFERENTE pero ya ta
 Invocador invocador = new Invocador();
 invocador.AgregarComando(new AgregarReservaCmd("Agregar Operador a Reserva", "Se lleva un operador a cuartel y se pone en reserva"));
 invocador.AgregarComando(new CargarSimulacionCmd("Cargar Simulacion", "Carga una simuacion antigua desde memoria"));
-invocador.AgregarComando(new ConfigurarSimulacionCmd("Configurar Nueva Simulacion", "Configura los parametros para una nueva simulacion"));
+invocador.AgregarComando(new ConfigExtensionZonalCmd("Configurar Extensiones Zonales", "Apartado para configurar las extensiones de cada zona"));
+invocador.AgregarComando(new ConfigExpansionZonalCmd("Configurar Expansiones Zonales", "Apartado para configurar las extensiones de cada zona"));
+invocador.AgregarComando(new ConfigPrioridadZonalCmd("Configurar Prioridades Zonales", "Apartado para configurar las prioridades de cada zona, es decir, en que orden se generan"));
+invocador.AgregarComando(new ConfigMaxAparicionZonalCmd("Configurar Cantidad Maxima de Aparcion Zonal", "Apartado para configurar la cantidad de iteraciones de cada zona"));
 invocador.AgregarComando(new CrearCuartelCmd("Crear Cuartel", "Se crea un nuevo cuartel en el mundo sin superar el maximo configurado"));
 invocador.AgregarComando(new CrearSimulacionCmd("Crear Simulacion", "Crea una nueva simulacion a partir de los parametros configurados"));
 invocador.AgregarComando(new DescargarEnCuartelCmd("Descargar Operador en Cuartel", "Se descarga la carga fisica del operador en su cuartel"));
@@ -39,6 +82,13 @@ Dictionary<string, string[]> opciones = new Dictionary<string, string[]>() {
         "Crear Simulacion",
         "Cargar Simulacion",
         "Configurar Nueva Simulacion"
+    }},
+    { "Configurar Nueva Simulacion", new string[] {
+        "Configurar Extensiones Zonales",
+        "Configurar Expansiones Zonales",
+        "Configurar Cantidad Maxima de Aparcion Zonal",
+        "Configurar Prioridades Zonales",
+        "Sky .Net Menu Simulaciones"
     }},
     { "Crear Simulacion", new string[] {
         "Crear Cuartel",
