6f49e92fdec73c3c44b8600ba22437f55fff2eda
Luciano <lumacias11702@gmail.com>
2023-11-28 10:56:21 -0300

    Create EnumOperadores.cs

---
+++ Entidades/Operadores/EnumOperadores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyNet.Entidades.Operadores
+{
+    public enum EnumOperadores
+    {
+        K9,
+        M8,
+        UAV
+    }
+}
